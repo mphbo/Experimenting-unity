ce08acbbc2be1117aa2d04702112ff0fe12d9175
Logan Thomas <logan.thomas@CO-VAN-MAC103.firmglobal.com>
2022-02-15 20:13:52 -0800

    first commit

---
+++ Assets/PrintToConsole.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintToConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("That's a far jump... and I do not like the look at of that ant over there!");    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
